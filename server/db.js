06e39728ce0d254b9374e25187268a0425f5e913
Roman Budde <roman_budde@hotmail.com>
2023-07-30 15:28:27 -0300

    db now points to prod

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    password: "123456",
-    host: "db",
+    user: "radmin",
+    password: "8RMIEw5NcniEIysFtKvwMnMZ6w3zdfr1",
+    host: "dpg-cj3aladiuie55pkcfv10-a.oregon-postgres.render.com",
     port: "5432",
-    database: "db_cuidadores"
+    database: "db_cuidadores",
+    ssl: true
 });
 
 module.exports = pool;
