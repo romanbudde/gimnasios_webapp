cecfdc2c84d22c523a835a0d35ed41b9f72eb655
Roman Budde <roman_budde@hotmail.com>
2023-08-01 11:44:28 -0300

    updated maps height

--- ReactCordova/src/components/VerMapaSede.js
+++ ReactCordova/src/components/VerMapaSede.js
@@ -73,7 +73,7 @@ const VerMapaSede = ({ sede, show, onClose }) => {
 
     const containerStyle = {
         width: '300px',
-        height: '600px'
+        height: '525px'
     };
 
     const blueDot = {
@@ -93,7 +93,7 @@ const VerMapaSede = ({ sede, show, onClose }) => {
             >
             </LoadScript> */}
 
-            <div className='fixed inset-0 bg-gray-800 bg-opacity-40 z-50 flex justify-center items-center'>
+            <div className='fixed inset-0 bg-gray-800 bg-opacity-40 z-50 flex justify-center items-center max-h-screen'>
                 <div className='flex flex-col relative w-5/6 max-h-screen'>
                     <button onClick={ onClose } type="button" className="absolute top-2 right-2 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
                         <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path></svg>
